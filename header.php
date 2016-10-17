12f1e905970de7b6589d59bb0be12c18591152fc
81jumpstr <activatorsmansion@gmail.com>
2016-10-17 14:53:54 +0200

    cleaned up, now calling wp action hooks via index.php

--- header.php
+++ header.php
@@ -6,39 +6,14 @@
     </div>
     <title><?php wp_title(); ?></title>
     
-
-    <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch|Dancing+Script|David+Libre|Open+Sans+Condensed:300|Poiret+One" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Wire+One" rel="stylesheet">
+    <!--google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Loved+by+the+King|Pontano+Sans|Wire+One" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Over+the+Rainbow" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Scope+One" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Waiting+for+the+Sunrise" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Pompiere" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Zeyada" rel="stylesheet">
-
+    
     <?php wp_head(); ?>
 
   </head>
 
   <body <?php body_class(); ?>>
-    <div class="heading">
-      <h1>Setupshop</h1>
-    </div>
 
 <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-
-    <div class="container">
-      <div class="description">
-        <h3>Project Name</h3>
-        <!--<p>A community is commonly considered a social unit (a group of people) who share something in common...</p>-->
-      </div>
-    
-      <div class="main">
-        <h3>Project Name</h2>
-        <!--<p>A community is commonly considered a social unit (a group of people) who share something in common, such as norms, values, identity, and often a sense of place that is situated in a given geographical area (e.g. a village, town, or neighborhood). Durable relations that extend beyond immediate genealogical ties also define a sense of community. People tend to define those social ties as important to their identity, practice, and roles in social institutions like family, home, work, government, society, or humanity, at large.[1][2][3] Although communities are usually small relative to personal social ties (micro-level), "community" may also refer to large group affiliations (or macro-level), such as national communities, international communities, and virtual communities.</p>-->
-      </div>
-    </div>
-
