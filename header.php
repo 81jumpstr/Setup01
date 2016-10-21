0519f25409d5083133c9cb355d68a741633112d7
81jumpstr <activatorsmansion@gmail.com>
2016-10-21 10:51:29 +0200

    mostly experimenting, fixing and spacing

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width">
-    <title><?php wp_title(''); ?></title>
+    <title><?php wp_title(); ?></title>
     
     <!--google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Loved+by+the+King|Pontano+Sans|Wire+One" rel="stylesheet">
@@ -12,10 +12,11 @@
     
     <?php wp_head(); ?>
 
+
   </head>
 
   <body <?php body_class(); ?>>
 
-    <!--<h1>Setupshop</h1>-->
+    <h1>Setupshop</h1>
 
     <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
