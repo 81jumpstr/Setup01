0c4b2b47e64d8874bc5776570c22fcf4088d4738
81jumpstr <activatorsmansion@gmail.com>
2016-10-31 15:53:35 +0200

    feat/style, header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width">
-    <title><?php wp_title(); ?></title>
+    <!--<title><?php wp_title(); ?></title>-->
     
     <!--google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Loved+by+the+King|Pontano+Sans|Wire+One" rel="stylesheet">
@@ -16,8 +16,8 @@
 
   <body <?php body_class(); ?>>
     
-    <section id='header'>
+    <header class="site-header">
       <h1>Setupshop</h1>
       <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-    </section>
+    </header>
     
