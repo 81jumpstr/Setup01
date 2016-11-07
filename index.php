5394c00f306986b066136aa0ff4d89a613d22783
81jumpstr <activatorsmansion@gmail.com>
2016-11-07 14:23:06 +0200

    removed class single-post from div, added to li, as advised

--- index.php
+++ index.php
@@ -4,14 +4,12 @@ get_sidebar();
 if ( have_posts() ) : 
 ?>
   <div class='post-section'>
-	  <ul>
-	      <div class='single-post'>
-		    <?php while ( have_posts() ) : the_post(); ?>
-			  <li>
-			    <?php the_content(); ?>
-			  </li>
-		    <?php endwhile; ?>
-		  </div>  
+	  <ul>   
+	    <?php while ( have_posts() ) : the_post(); ?>
+		  <li class='single-post'>
+		    <?php the_content(); ?>
+		  </li>
+	    <?php endwhile; ?>  
 	  </ul>
   </div>
 
