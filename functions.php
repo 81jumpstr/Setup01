29a94f087bef9da56a21886aee7c6025352331f6
81jumpstr <activatorsmansion@gmail.com>
2016-11-07 16:40:27 +0200

    removed unnecessary comment

--- functions.php
+++ functions.php
@@ -8,8 +8,6 @@
 
 ?>
 
-
-
 <?php
 
   /**  ------- my additions below --------
@@ -37,22 +35,3 @@ function register_my_menu() {
 }
 add_action( 'init', 'register_my_menu' );
 ?>
-
-<!--<?php
-add_filter( 'wp_title', 'wpdocs_hack_wp_title_for_home' );
-
-/**
- * Customize the title for the home page, if one is not set.
- *
- * @param string $title The original title.
- * @return string The title to use.
- */
-function wpdocs_hack_wp_title_for_home( $title )
-{
-  if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
-    $title = __( 'Home', 'textdomain' ) . ' | ' . get_bloginfo( 'description' );
-  }
-  return $title;
-}
-?>
--->
