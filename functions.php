352e7f70e67050c5dec970fe7583ffdac7dee274
81jumpstr <activatorsmansion@gmail.com>
2016-11-11 12:00:47 +0200

    thumbnail support

--- functions.php
+++ functions.php
@@ -35,3 +35,10 @@ function register_my_menu() {
 }
 add_action( 'init', 'register_my_menu' );
 ?>
+
+<!-- adding and sizing - posts with thumbnails -->
+
+<?php
+  add_theme_support( 'post-thumbnails' );
+  set_post_thumbnail_size( 100, 50, true );
+?>
