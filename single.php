a1a3a9bf15215c85fde03fb24cfb771d17daa899
81jumpstr <activatorsmansion@gmail.com>
2016-11-14 16:29:56 +0200

    syntax alignment

--- single.php
+++ single.php
@@ -6,22 +6,26 @@
 	  <ul class="posts">
 		<?php 
 		  if ( have_posts() ) :
-	      	while ( have_posts() ) : the_post();
+	      while ( have_posts() ) : the_post();
 		?>
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+		<?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
-			<?php if( has_post_thumbnail() ): ?>
-				<div class="pull-right"><?php the_post_thumbnail('thumbnail'); ?></div>
-			<?php endif; ?>
-
-			<small><?php the_category(); ?></small>
-
-		    <div>
-			  <?php the_content(); ?>
+		<?php if( has_post_thumbnail() ): ?>
+			<div class="pull-right">
+				<?php the_post_thumbnail('thumbnail'); ?>
 			</div>
+		<?php endif; ?>
+
+		<small>
+			<?php the_category(); ?>
+		</small>
+
+		<div>
+		  <?php the_content(); ?>
+		</div>
 
 		<?php 
 			endwhile;
