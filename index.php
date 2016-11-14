79b21de1c76e65f54f25258991cd78a227d48e7d
81jumpstr <activatorsmansion@gmail.com>
2016-11-14 17:21:31 +0200

    added theme support/thumbnail function

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
+	
+<!-- adding and sizing - posts with thumbnails -->
+
+	<?php
+	  add_action('init', 'setupshop_theme_init');
+		function setupshop_theme_init() {
+	  add_theme_support( 'post-thumbnails' ); 
+	  set_post_thumbnail_size( 100, 50, true );
+	} 
+	?>
 
 	<div class='main-section'>
-	<?php get_sidebar(); ?>
+	  <?php get_sidebar(); ?>
 	  <ul class="posts">
 		<?php 
 		  if ( have_posts() ) :
