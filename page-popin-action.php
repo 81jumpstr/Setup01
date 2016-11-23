3102215ef8a2a5b57b5e31129ab629bc73948509
81jumpstr <activatorsmansion@gmail.com>
2016-11-23 13:36:21 +0200

    fixed indentation

--- page-popin-action.php
+++ page-popin-action.php
@@ -1,19 +1,21 @@
 <?php get_header(); ?>
 
-	<div class='main-section'>
-	  <ul class="posts">
-		<?php 
-		  if ( have_posts() ) :
-	      	while ( have_posts() ) : the_post();
-		?>      
-	    <li>
-		  <?php the_content('&raquo;'); ?>
-		</li>
-		<?php 
-			endwhile;
-			endif;
-		?>
-	  </ul>
-	</div>
+<div class='main-section'>
+  <ul class="posts">
+    <?php 
+      if ( have_posts() ) :
+      while ( have_posts() ) : the_post();
+    ?>
+      
+    <li>
+      <?php the_content('&raquo;'); ?>
+    </li>
+
+    <?php 
+      endwhile;
+      endif;
+    ?>
+  </ul>
+</div>
 
 <?php get_footer(); ?>
