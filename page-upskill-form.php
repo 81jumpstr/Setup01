9fafba0f4705225c97aa607b594a6d7facf6fc9c
81jumpstr <activatorsmansion@gmail.com>
2016-11-24 15:18:53 +0200

    added php file for form

---
+++ page-upskill-form.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+<form>
+  <h1>1UP</h1>
+  <ul class="1up-details">
+    <li>
+	    <label for="bizprojectname">Business/Project Name:</label>
+	    <input type="text" name="bizprojectname">
+	  </li>
+	  <li>  
+	    <label for="url">Website:</label>
+	    <input type="text" name="website">
+    </li>
+    <li>  
+	    <label for="url">Demoable Website:</label>
+	    <input type="text" name="demosite">
+    </li>
+    <li>
+      <label for="firstname">Contact First Name:</label>
+      <input type="name" name="contact-firstname">
+    </li>
+    <li>
+      <label for="lastname">Contact Last Name:</label>
+      <input type="name" name="contact-lastname">
+    </li>
+    <li>
+      <label for="url">Contact website/email address:</label>
+      <input type="name" name="contact-url">
+    </li>
+    <li>
+      <label for="description">Description:</label>
+	    <textarea class="description" type="text" alt="Brief description of your product/service (140 characters max)...">
+	    </textarea>
+    </li>
+  </ul>
+
+  <div class="1up-radios">
+	  <input type="radio" name="description" value="project"> Intervention (not for profit)
+	  <input type="radio" name="description" value="project"> Project (for profit)
+	  <input type="radio" name="description" value="business"> Business (fundraiser)
+	  <input type="radio" name="description" value="business"> Business (for profit)
+  </div>
+  
+  <input class="1up-submit" type="submit" value="Submit">
+
+</form>
+
+<?php get_footer(); ?>
