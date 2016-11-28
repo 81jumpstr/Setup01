244998471089f6100ceb0be308c69d34b97fa63d
81jumpstr <activatorsmansion@gmail.com>
2016-11-28 15:19:28 +0200

    <li> PR Corrections - 2

--- single.php
+++ single.php
@@ -2,35 +2,34 @@
 
   <div class='main-section'>
     <?php get_sidebar(); ?>
-    <ul class="posts">
-      <?php 
-        if ( have_posts() ) :
-          while ( have_posts() ) : the_post();
-      ?>
+    <?php 
+      if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+    ?>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
-            <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+          <?php if( has_post_thumbnail() ): ?>
+            <div class="thumbnails">
+              <?php the_post_thumbnail('thumbnail'); ?>
+            </div>
+          <?php endif; ?>
 
-            <?php if( has_post_thumbnail() ): ?>
-            	<div class="thumbnails">
-                <?php the_post_thumbnail('thumbnail'); ?>
-              </div>
-            <?php endif; ?>
-
-            <small>
-              <?php the_category(); ?>
-            </small>
-
-      <li>
-        <?php the_content(); ?>
-      </li>
-
-      <?php 
-          endwhile;
-        endif;
-      ?>
-    </ul>
+          <small>
+            <?php the_category(); ?>
+          </small>
+          <div>
+            <ul class="posts">
+              <li>
+                <?php the_content(); ?>
+              </li>
+            </ul>
+          </div>
+    <?php 
+        endwhile;
+      endif;
+    ?>
   </div>
 
 <?php get_footer(); ?>
