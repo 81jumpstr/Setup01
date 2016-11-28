1f815182759ac087332c6e99d9764e376910a5f2
81jumpstr <activatorsmansion@gmail.com>
2016-11-28 11:44:27 +0200

    fixed indentation

--- single.php
+++ single.php
@@ -3,28 +3,28 @@
 	<div class='main-section'>
 	  <?php get_sidebar(); ?>
 	  <ul class="posts">
-		<?php 
-		  if ( have_posts() ) :
+		  <?php 
+		    if ( have_posts() ) :
 	      while ( have_posts() ) : the_post();
-		?>
+		  ?>
 
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+		    <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
-		<?php if( has_post_thumbnail() ): ?>
-			<div class="thumbnails">
-				<?php the_post_thumbnail('thumbnail'); ?>
-			</div>
-		<?php endif; ?>
+		    <?php if( has_post_thumbnail() ): ?>
+			  <div class="thumbnails">
+				  <?php the_post_thumbnail('thumbnail'); ?>
+			  </div>
+        <?php endif; ?>
 
-		<small>
+		  <small>
 			<?php the_category(); ?>
-		</small>
+		  </small>
 
-		<div>
-		  <?php the_content(); ?>
-		</div>
+		  <li>
+		    <?php the_content(); ?>
+      </li>
 
 		<?php 
 			endwhile;
