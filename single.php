74639a75ef8cb766f6d8cd75e0abde6eeb35d8ad
81jumpstr <activatorsmansion@gmail.com>
2016-11-28 15:28:46 +0200

    PR corrections, removed article id - 3

--- single.php
+++ single.php
@@ -6,7 +6,7 @@
       if ( have_posts() ) :
         while ( have_posts() ) : the_post();
     ?>
-          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <article <?php post_class(); ?>>
 
           <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
