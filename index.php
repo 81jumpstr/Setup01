f92ca43eaf19ee7741e3885e8fb8f25d69e2a36e
81jumpstr <activatorsmansion@gmail.com>
2016-11-30 12:40:44 +0200

    fixed indentation

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php get_header(); ?>
 
-	<div class='main-section'>
-	  <?php get_sidebar(); ?>
-	  <ul class="posts">
-		<?php 
-		  if ( have_posts() ) :
-	      	while ( have_posts() ) : the_post();
-		?>      
-	    <li>
-		  <?php the_content('&raquo;'); ?>
-		</li>
-		<?php 
-			endwhile;
-			endif;
-		?>
-	  </ul>
-	</div>
+  <div class='main-section'>
+    <?php get_sidebar(); ?>
+    <ul class="posts">
+      <?php 
+        if ( have_posts() ) :
+          while ( have_posts() ) : the_post();
+      ?>
+      <li>
+        <?php the_content('&raquo;'); ?>
+      </li>
+      <?php 
+          endwhile;
+        endif;
+      ?>
+    </ul>
+  </div>
 
 <?php get_footer(); ?>
