2ff6f0e34c0e9e686caa2ef18e25d3ee9694a4fe
81jumpstr <activatorsmansion@gmail.com>
2016-11-30 15:18:24 +0200

    modified/untracked file, in case I haven't pushed changes

--- page.php
+++ page.php
@@ -1,36 +1,34 @@
 <?php get_header(); ?>
 
-	<div class='main-section'>
-	  <ul class="posts">
-		<?php 
-		  if ( have_posts() ) :
-	      while ( have_posts() ) : the_post();
-			get_template_part( 'content', 'page' );
-		?>
+  <div class='main-section'>
+    <ul class="posts">
+      <?php 
+        if ( have_posts() ) :
+          while ( have_posts() ) : the_post();
+        get_template_part( 'content', 'page' );
+      ?>
 
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+      <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
-		<?php if( has_post_thumbnail() ): ?>
-			<div class="thumbnails">
-				<?php the_post_thumbnail('thumbnail'); ?>
-			</div>
-		<?php endif; ?>
+      <?php if( has_post_thumbnail() ): ?>
+      <div class="thumbnails">
+        <?php the_post_thumbnail('thumbnail'); ?>
+      </div>
+      <?php endif; ?>
 
-		<small>
-			<?php the_category(); ?>
-		</small>
+      <small>
+        <?php the_category(); ?>
+      </small>
 
-		<div>
-		  <?php the_content(); ?>
-		</div>
+      <?php the_content(); ?>
 
-		<?php 
-			endwhile;
-			endif;
-		?>
-	  </ul>
-	</div>
+      <?php 
+          endwhile;
+        endif;
+      ?>
+    </ul>
+  </div>
 
 <?php get_footer(); ?>
