e176149a58ac74004ac2b864a7c1378efb667525
81jumpstr <activatorsmansion@gmail.com>
2016-12-01 16:23:30 +0200

    changes not done, changing branches to check something, ignore

--- page.php
+++ page.php
@@ -1,14 +1,16 @@
 <?php get_header(); ?>
 
   <div class='main-section'>
-    <ul class="posts">
       <?php 
         if ( have_posts() ) :
-          while ( have_posts() ) : the_post();
-        get_template_part( 'content', 'page' );
       ?>
+          <ul class="posts">
+          <?php
+            while ( have_posts() ) : the_post();
+              get_template_part( 'content', 'page' );
+          ?>
 
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
       <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
