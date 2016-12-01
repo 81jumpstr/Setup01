9acaabb4039a470e009ea654c93cd1f15678ba9b
81jumpstr <activatorsmansion@gmail.com>
2016-12-01 16:53:24 +0200

    fixed indentation and ul

--- page.php
+++ page.php
@@ -1,36 +1,44 @@
 <?php get_header(); ?>
 
   <div class='main-section'>
-      <?php 
-        if ( have_posts() ) :
+    <?php 
+      if ( have_posts() ) :
+    ?>
+        <ul class="posts">
+      <?php
+        while ( have_posts() ) : the_post();
+          get_template_part( 'content', 'page' );
       ?>
-          <ul class="posts">
-          <?php
-            while ( have_posts() ) : the_post();
-              get_template_part( 'content', 'page' );
-          ?>
-
           <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-      <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+          <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
-      <?php if( has_post_thumbnail() ): ?>
-      <div class="thumbnails">
-        <?php the_post_thumbnail('thumbnail'); ?>
-      </div>
-      <?php endif; ?>
+        <?php 
+          if( has_post_thumbnail() ): 
+        ?>
+            <div class="thumbnails">
+              <?php the_post_thumbnail('thumbnail'); ?>
+            </div>
+        <?php 
+          endif; 
+        ?>
 
-      <small>
-        <?php the_category(); ?>
-      </small>
+          <small>
+            <?php the_category(); ?>
+          </small>
 
-      <?php the_content(); ?>
+            <li>
+              <?php the_content(); ?>
+            </li>
 
       <?php 
-          endwhile;
-        endif;
+        endwhile;
       ?>
-    </ul>
+
+        </ul>
+    <?php
+      endif;
+    ?>
   </div>
 
 <?php get_footer(); ?>
