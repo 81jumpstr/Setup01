fe26db80c7590b4eeb00313c90d2680dd175afb7
81jumpstr <activatorsmansion@gmail.com>
2016-12-02 10:27:59 +0200

    fixed ul, li, indentation

--- page-upskill.php
+++ page-upskill.php
@@ -7,37 +7,41 @@
 
 <?php get_header(); ?>
 
-	<div class='main-section'>
-	  <ul class="posts">
+  <div class='main-section'>
+    <?php 
+      if ( have_posts() ) :
+    ?>
+        <ul class="posts">
+      <?php
+        while ( have_posts() ) : the_post();
+          get_template_part( 'content', 'page' );
+      ?>
+
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+          <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+
+    <?php if( has_post_thumbnail() ): ?>
+      <div class="thumbnails">
+        <?php the_post_thumbnail('thumbnail'); ?>
+      </div>
+    <?php endif; ?>
+
+          <small>
+            <?php the_category(); ?>
+          </small>
+
+          <li>
+            <?php the_content(); ?>
+          </li>
+
 		<?php 
-		  if ( have_posts() ) :
-	      while ( have_posts() ) : the_post();
-			get_template_part( 'content', 'page' );
-		?>
-
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-		<?php the_title('<h1 class="entry-title">','</h1>' ); ?>
-
-		<?php if( has_post_thumbnail() ): ?>
-			<div class="thumbnails">
-				<?php the_post_thumbnail('thumbnail'); ?>
-			</div>
-		<?php endif; ?>
-
-		<small>
-			<?php the_category(); ?>
-		</small>
-
-		<div>
-		  <?php the_content(); ?>
-		</div>
-
-		<?php 
-			endwhile;
+			  endwhile;
+    ?>
+        </ul>
+    <?php
 			endif;
 		?>
-	  </ul>
 	</div>
 
 <?php get_footer(); ?>
