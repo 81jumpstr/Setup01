93ffe64ba6eaa145eb80476c8809bed0dc6c5240
81jumpstr <activatorsmansion@gmail.com>
2016-12-02 12:45:26 +0200

    mooved block of ul up to 1st if statement

--- page-upskill.php
+++ page-upskill.php
@@ -10,27 +10,30 @@
   <div class='main-section'>
     <?php 
       if ( have_posts() ) :
+    ?>
+        <ul class="posts">
+          <li>
+            <?php the_content(); ?>
+          </li>
+        </ul>
+
+      <?php
         while ( have_posts() ) : the_post();
           get_template_part( 'content', 'page' );
-    ?>
+      ?>
           <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
           <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
     <?php if( has_post_thumbnail() ): ?>
-      <div class="thumbnails">
-        <?php the_post_thumbnail('thumbnail'); ?>
-      </div>
+            <div class="thumbnails">
+              <?php the_post_thumbnail('thumbnail'); ?>
+            </div>
     <?php endif; ?>
 
           <small>
             <?php the_category(); ?>
           </small>
-          <ul class="posts">
-            <li>
-              <?php the_content(); ?>
-            </li>
-          </ul>
     <?php 
 			  endwhile;
       endif;
