0352f05b9a6fce96e74bf9c4177cec8b70255bc3
81jumpstr <activatorsmansion@gmail.com>
2016-12-06 14:36:41 +0200

    added ul and li, fixed indentation, removed sidebar

--- page-about.php
+++ page-about.php
@@ -8,31 +8,38 @@
 <?php get_header(); ?>
 
   <div class='main-section'>
-    <?php get_sidebar(); ?>
-    <?php 
-      if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
+  <?php 
+    if ( have_posts() ) :
+  ?>
+      <ul class="posts">
+    <?php
+      while ( have_posts() ) : the_post();
     ?>
-          <article <?php post_class(); ?>>
-
-          <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
-
-          <?php if( has_post_thumbnail() ): ?>
-            <div class="thumbnails">
-              <?php the_post_thumbnail('thumbnail'); ?>
-            </div>
-          <?php endif; ?>
+        <li>
+          <article <?php the_title('<h1 class="entry-title">','</h1>' ); post_class(); ?>>
+      <?php 
+        if( has_post_thumbnail() ): 
+      ?>
+          <div class="thumbnails">
+            <?php the_post_thumbnail('thumbnail'); ?>
+          </div>
+      <?php 
+        endif; 
+      ?>
 
           <small>
             <?php the_category(); ?>
           </small>
 
           <?php the_content(); ?>
-
+        </li>
     <?php 
-        endwhile;
-      endif;
+      endwhile;
     ?>
+      </ul>
+  <?php
+    endif;
+  ?>
   </div>
 
 <?php get_footer(); ?>
