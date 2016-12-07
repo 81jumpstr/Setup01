6a9e99de6b24ac29dbb07ebe80695233e0db5c2b
81jumpstr <activatorsmansion@gmail.com>
2016-12-07 16:04:38 +0200

    NO SIDEBAR, removed ul-li, fixed indentation

--- page-about.php
+++ page-about.php
@@ -8,39 +8,33 @@
 <?php get_header(); ?>
 
   <div class='main-section'>
-  <?php 
-    if ( have_posts() ) :
-  ?>
-      <ul class="posts">
-    <?php
-      while ( have_posts() ) : the_post();
+    <?php 
+      if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
     ?>
-        <li>
           <article <?php post_class(); ?>>
           <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
-      <?php 
-        if( has_post_thumbnail() ): 
-      ?>
-          <div class="thumbnails">
-            <?php the_post_thumbnail('thumbnail'); ?>
-          </div>
-      <?php 
-        endif; 
-      ?>
 
-          <small>
-            <?php the_category(); ?>
-          </small>
+        <?php 
+          if( has_post_thumbnail() ): 
+        ?>
+            <div class="thumbnails">
+              <?php the_post_thumbnail('thumbnail'); ?>
+            </div>
+        <?php 
+          endif; 
+        ?>
 
-          <?php the_content(); ?>
-        </li>
+        <small>
+          <?php the_category(); ?>
+        </small>
+
+        <?php the_content(); ?>
+
     <?php 
-      endwhile;
+        endwhile;
+      endif;
     ?>
-      </ul>
-  <?php
-    endif;
-  ?>
   </div>
 
 <?php get_footer(); ?>
