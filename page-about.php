49d1b8ac16ef4646806415f07ae1329d606ecc92
81jumpstr <activatorsmansion@gmail.com>
2016-12-08 11:08:17 +0200

    adding contacts and social media click-through images

--- page-about.php
+++ page-about.php
@@ -1,95 +1,83 @@
 <?php 
 /*
  *    Template Name: Page-About
- *    Description: About site, Contacts Us, the Team combined
+ *    Description: About site and Contacts Us, combined
  */
 ?>
 
 <?php get_header(); ?>
 
   <div class='main-section'>
+
     <?php 
       if ( have_posts() ) :
         while ( have_posts() ) : the_post();
     ?>
-          <article <?php post_class(); ?>>
-          <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+          <article <?php post_class(); ?>>         
+            <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
-        <?php 
-          if( has_post_thumbnail() ): 
-        ?>
-            <div class="thumbnails">
-              <?php the_post_thumbnail('thumbnail'); ?>
-            </div>
-        <?php 
-          endif; 
-        ?>
-        
-        <small>
-          <?php the_category(); ?>
-        </small>
+          <?php 
+            if( has_post_thumbnail() ): ?>
+              <div class="thumbnails">
+                <?php the_post_thumbnail('thumbnail'); ?>
+              </div>
+          <?php 
+            endif; 
+          ?>
 
-          <div class="team">
-            <ul>
-              <div class="tmember01">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember06.jpg" alt="contacts-image01">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember02">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember05.jpg" alt="contacts-image02">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember03">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember04.jpg" alt="contacts-image03">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember04">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember03.jpg" alt="contacts-image04">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember05">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember02.jpg" alt="contacts-image05">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember06">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember01.jpg" alt="contacts-image06">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-            </ul>      
-          </div>
+            <small>
+              <?php the_category(); ?>
+            </small>
 
-        <?php the_content(); ?>
+            <ul class="contacts-socialmedia">
+              <li>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tel.png" alt="contacts-tel-image" />
+                <p>Tel. #:</p>
+              </li>
+              <li>
+                <a href="https://www.gmail.com">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/email.png" alt="contacts-email-image" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook.png" alt="contacts-facebook-image" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.twitter.com">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.png" alt="contacts-twitter-image" />
+                </a>
+              </li>
+              <li class="contacts-linkedin">
+                <a href="https://www.linkedin.com">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/linkedin.png" alt="contacts-linkedin-image" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/instagram.png" alt="contacts-instagram-image" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/youtube.png" alt="contacts-youtube-image" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.google.com">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/gplus.png" alt="contacts-gplus-image" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.github.com">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/github.png" alt="contacts-github-image" />
+                </a>
+            </li>
+          </ul>
 
+          </article>
+          <?php the_content(); ?>
     <?php 
         endwhile;
       endif;
