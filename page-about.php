5ac6b6ccf4682d820036730d09668fb257fd42b2
81jumpstr <activatorsmansion@gmail.com>
2016-12-08 10:20:10 +0200

    removed li-divs, fixed indentation, renamed ul-class

--- page-about.php
+++ page-about.php
@@ -13,83 +13,63 @@
         while ( have_posts() ) : the_post();
     ?>
           <article <?php post_class(); ?>>
-          <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+            <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
-        <?php 
-          if( has_post_thumbnail() ): 
-        ?>
-            <div class="thumbnails">
-              <?php the_post_thumbnail('thumbnail'); ?>
-            </div>
-        <?php 
-          endif; 
-        ?>
+            <?php 
+              if( has_post_thumbnail() ): 
+            ?>
+                <div class="thumbnails">
+                  <?php the_post_thumbnail('thumbnail'); ?>
+                </div>
+            <?php 
+              endif; 
+            ?>
         
-        <small>
-          <?php the_category(); ?>
-        </small>
+            <small>
+              <?php the_category(); ?>
+            </small>
 
-          <div class="team">
-            <ul>
-              <div class="tmember01">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember06.jpg" alt="contacts-image01">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember02">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember05.jpg" alt="contacts-image02">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember03">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember04.jpg" alt="contacts-image03">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember04">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember03.jpg" alt="contacts-image04">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember05">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember02.jpg" alt="contacts-image05">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-              <div class="tmember06">
-                <li>
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember01.jpg" alt="contacts-image06">
-                  <p>Name:</p>
-                  <p>Porfolio:</p>
-                  <p>Quote:</p>
-                  <hr>
-                </li>
-              </div>
-            </ul>      
-          </div>
+            <ul class="image-detailed">
+              <li>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember06.jpg" alt="contacts-image01">
+                <p>Name:</p>
+                <p>Porfolio:</p>
+                <p>Quote:</p>
+              </li>
+              <li>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember05.jpg" alt="contacts-image02">
+                <p>Name:</p>
+                <p>Porfolio:</p>
+                <p>Quote:</p>
+              </li>
+              <li>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember04.jpg" alt="contacts-image03">
+                <p>Name:</p>
+                <p>Porfolio:</p>
+                <p>Quote:</p>
+              </li>
+              <li>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember03.jpg" alt="contacts-image04">
+                <p>Name:</p>
+                <p>Porfolio:</p>
+                <p>Quote:</p>
+              </li>
+              <li>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember02.jpg" alt="contacts-image05">
+                <p>Name:</p>
+                <p>Porfolio:</p>
+                <p>Quote:</p>
+              </li>
+              <li>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tmember01.jpg" alt="contacts-image06">
+                <p>Name:</p>
+                <p>Porfolio:</p>
+                <p>Quote:</p>
+              </li>
+            </ul>
 
-        <?php the_content(); ?>
-
+            <?php the_content(); ?>
+          </article>
     <?php 
         endwhile;
       endif;
