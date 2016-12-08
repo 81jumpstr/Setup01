4b7ecf147330100dc350de20208026a9ad56f6b4
81jumpstr <activatorsmansion@gmail.com>
2016-12-08 14:59:16 +0200

    closed article and fixed indentation >> merge mishap fix

--- page.php
+++ page.php
@@ -6,24 +6,20 @@
         while ( have_posts() ) : the_post();
     ?>
           <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
 
-          <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
-
-        <?php 
-          if( has_post_thumbnail() ): 
-        ?>
-            <div class="thumbnails">
-              <?php the_post_thumbnail('thumbnail'); ?>
-            </div>
-        <?php 
-          endif; 
-        ?>
+            <?php if( has_post_thumbnail() ): ?>
+              <div class="thumbnails">
+                <?php the_post_thumbnail('thumbnail'); ?>
+              </div>
+            <?php endif; ?>
         
-          <small>
-            <?php the_category(); ?>
-          </small>
+            <small>
+              <?php the_category(); ?>
+            </small>
 
-          <?php the_content(); ?>
+            <?php the_content(); ?>
+          </article>
 
     <?php 
         endwhile;
